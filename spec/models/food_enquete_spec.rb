7bf9e725f0301c0af67e6657ac570b9d7eb0487e
asahi <asap1017@docomo.ne.jp>
2021-07-15 10:14:25 +0900

    前処理の共通化

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -83,10 +83,12 @@ RSpec.describe FoodEnquete, type: :model do
 
   describe 'アンケート回答時の条件' do
     context 'メールアドレスを確認すること' do
-      it '同じメールアドレスで再び回答できないこと' do
-        # [Point.3-6-1]1つ目のテストデータを作成します。
+
+      before do
         FactoryBot.create(:food_enquete_tanaka)
-  
+      end
+
+      it '同じメールアドレスで再び回答できないこと' do
         # [Point.3-6-2]2つ目のテストデータを作成します。
         re_enquete_tanaka = FactoryBot.build(:food_enquete_tanaka, food_id: 0, score: 1, present_id: 0, request: "スープがぬるかった")
         expect(re_enquete_tanaka).not_to be_valid
@@ -98,7 +100,6 @@ RSpec.describe FoodEnquete, type: :model do
         expect(FoodEnquete.all.size).to eq 1
       end
       it '異なるメールアドレスで回答できること' do
-        FactoryBot.create(:food_enquete_tanaka)
         enquete_yamada = FactoryBot.build(:food_enquete_yamada)
 
         expect(enquete_yamada).to be_valid
